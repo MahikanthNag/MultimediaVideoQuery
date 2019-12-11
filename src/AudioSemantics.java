ab38d7ea08c7c6f9fe84d19d6d5de1f72996e295
Srikar <srikarrampally@gmail.com>
2019-12-10 19:39:21 -0800

    AudioSemantics

--- src/AudioSemantics.java
+++ src/AudioSemantics.java
@@ -33,14 +33,30 @@ public class AudioSemantics {
 	Map<String, Double> similarities = new HashMap<>();
 	
 	public Map<String, Double> calculateStatsOfAllPairs(String queryPath) throws ClassNotFoundException, IOException {
-		similarities.put("flowers", calculateSimilarity("flowers", queryPath));
-		similarities.put("interview", calculateSimilarity("interview", queryPath));
-		similarities.put("movie", calculateSimilarity("movie", queryPath));
-		similarities.put("musicvideo", calculateSimilarity("musicvideo", queryPath));
-		similarities.put("sports", calculateSimilarity("sports", queryPath));
-		similarities.put("starcraft", calculateSimilarity("starcraft", queryPath));
-		similarities.put("traffic", calculateSimilarity("traffic", queryPath));
+		Map<Integer, Double> distances = new HashMap<>();
+		String[] videoNames = {"flowers", "interview", "movie", "musicvideo", "sports", "starcraft", "traffic"};
+		distances.put(0, calculateSimilarity("flowers", queryPath));
+		distances.put(1, calculateSimilarity("interview", queryPath));
+		distances.put(2, calculateSimilarity("movie", queryPath));
+		distances.put(3, calculateSimilarity("musicvideo", queryPath));
+		distances.put(4, calculateSimilarity("sports", queryPath));
+		distances.put(5, calculateSimilarity("starcraft", queryPath));
+		distances.put(6, calculateSimilarity("traffic", queryPath));
 		
+		double maxDist = 0, minDist = 9999999;
+		for (int i = 0; i < distances.size(); i++)
+		{
+			double val = distances.get(i);
+			if (val < minDist)
+				minDist = val;
+			if (val > maxDist)
+				maxDist = val;
+		}
+		for (int i = 0; i < distances.size(); i++)
+		{
+			double simVal = 100 - ((distances.get(i) - minDist)/(maxDist - minDist)*100);
+			similarities.put(videoNames[i], Constants.CONTRAST_PRIORITY * simVal);
+		}
 		return similarities;
 	}
 	
@@ -57,40 +73,24 @@ public class AudioSemantics {
 			
 			int readBytes = 0;
 			int framesRead = 0;
-			ArrayList<Double> frameValues = new ArrayList<>();
+			ArrayList<Integer> frameValues = new ArrayList<>();
 			ArrayList<Double> frameValuesWithRespectToVideo = new ArrayList<>();
+
+			readBytes = audioInputStream.read(audioBuffer, 0, audioBuffer.length);
+			framesRead += readBytes/audioFormat.getFrameSize();
 			
-			while (readBytes != -1) {
-				readBytes = audioInputStream.read(audioBuffer, 0, audioBuffer.length);
-				framesRead += readBytes/audioFormat.getFrameSize();
-			}
-//			int sum = 0;
-//			double avgValue = 0.0;
-//			double sumValue = 0.0;
-//			
-//			for(int i = 0; i < audioBuffer.length; i++) {
-//				sum += audioBuffer[i];
-//			}
-//			avgValue = sum/audioBuffer.length;
-//			
-//			for(int j = 0; j < audioBuffer.length; j++) {
-//				sumValue += Math.pow(audioBuffer[j] - avgValue, 2); 
-//			}
-//			
-//			return sumValue/audioBuffer.length;
-			
-			for(int i = 0; i < framesRead; i++) {
-				double value = (double) ((audioBuffer[i * 2] & 0xff) + (audioBuffer[i * 2 + 1] << 8));
-				frameValues.add(value);
+			for(int i = 0; i < readBytes; i++) {
+//				double value = (double) ((audioBuffer[i * 2] & 0xff) + (audioBuffer[i * 2 + 1] << 8));
+				frameValues.add(overcomeByteRangeeError(audioBuffer[i]));
 			}
 			
-			int ratio = Constants.AUDIO_FRAME_RATE / Constants.FRAME_RATE; 
+			int ratio = Constants.AUDIO_FRAME_RATE / 60; 
 			
-			int lengthOfAudio = framesRead / Constants.AUDIO_FRAME_RATE * Constants.FRAME_RATE;
+			int lengthOfAudio = readBytes / Constants.AUDIO_FRAME_RATE * 60;
             for (int i = 0; i < lengthOfAudio; i++) {
                 double sum = 0;
                 for (int j = 0; j < ratio; j++) {
-                    sum += Math.abs(frameValues.get(i * ratio + j));
+                    sum += frameValues.get(i * ratio + j);
                 }
                 frameValuesWithRespectToVideo.add(sum);
             }
@@ -114,9 +114,9 @@ public class AudioSemantics {
 		
         Entry<Integer, Double> minError = calculateMinError(queryFrames, databaseAudioFrames);
         
-        double matchPercentage = 100 - Math.round(minError.getValue()*10000.0)/100.0;
+//        double matchPercentage = 100 - Math.round(minError.getValue()*10000.0)/100.0;
         
-		return Constants.AUDIO_PRIORITY * matchPercentage;
+		return Constants.AUDIO_PRIORITY * minError.getValue();
 				
  		
 	}
@@ -124,15 +124,32 @@ public class AudioSemantics {
 	public Entry<Integer, Double> calculateMinError(ArrayList<Double> queryFrames, ArrayList<Double> databaseAudioFrames) {
 		
 		Map<Integer, Double> errorValues = new HashMap<>();
-		for(int i = 0; i < databaseAudioFrames.size() - queryFrames.size(); i++) {
-			double sum = 0.0;
-			double databaseSum = 0.0;
-			for(int j = 0; j < queryFrames.size(); j++) {
-				sum += Math.abs(databaseAudioFrames.get(i + j) - queryFrames.get(j));
-				databaseSum += databaseAudioFrames.get(i + j);
+//		for(int i = 0; i < databaseAudioFrames.size() - queryFrames.size(); i++) {
+//			double sum = 0.0;
+//			double databaseSum = 0.0;
+//			for(int j = 0; j < queryFrames.size(); j++) {
+//				sum += Math.abs(databaseAudioFrames.get(i + j) - queryFrames.get(j));
+//				databaseSum += Math.abs(databaseAudioFrames.get(i + j));
+//			}
+//			errorValues.put(i, Math.min(sum, databaseSum)/Math.max(sum, databaseSum));
+//		}
+		int chunckSize = 100;
+		for(int i = 0; i < queryFrames.size() - chunckSize; i++) {
+			ArrayList<Double> chunkOfQueryFrame = new ArrayList<Double>(queryFrames.subList(i, i + chunckSize));
+			double minVal = Integer.MAX_VALUE;
+			for(int j = 0; j < databaseAudioFrames.size() - chunckSize; j++) {
+				ArrayList<Double> chunkOfDBFrame = new ArrayList<Double>(databaseAudioFrames.subList(j, j + chunckSize));
+				double sum = 0.0;
+				for(int k = 0; k < chunckSize; k++) {
+					sum += Math.abs(chunkOfQueryFrame.get(k) - chunkOfDBFrame.get(k));
+				}
+				if(sum < minVal) {
+					minVal = sum;
+				}
 			}
-			errorValues.put(i, Math.min(sum, databaseSum)/Math.max(sum, databaseSum));
+			errorValues.put(i, minVal);
 		}
+		
 		
 		return calculateMinEntry(errorValues);
 	}
@@ -153,6 +170,11 @@ public class AudioSemantics {
         ObjectOutputStream oos = new ObjectOutputStream(fos);
         oos.writeObject(audioVariance);        
         oos.close();
+	}
+	
+	public int overcomeByteRangeeError(byte b) {
+		if(b < 0) return (int)b + 256;
+		else return (int)b;
 	}
 
 }
