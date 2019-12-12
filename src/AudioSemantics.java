013d32e49e05c7039a2912715ce93b878b5a76e0
Srikar <srikarrampally@gmail.com>
2019-12-12 00:32:08 -0800

    Added fixes to Audio

--- src/AudioSemantics.java
+++ src/AudioSemantics.java
@@ -5,9 +5,11 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Random;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
@@ -20,14 +22,18 @@ public class AudioSemantics {
 	private int minValue = Integer.MAX_VALUE;
 	private int maxValue = Integer.MIN_VALUE;
 	ArrayList<Double> audioVariance;
-	Map<Integer, Double> errorValues = new HashMap<>();
-	
-	
-	public Map<Integer, Double> framewiseAudioValues = new HashMap<>();
+
+	public HashMap<String, HashMap<Integer, Double>> framewiseAudioValues = new HashMap<>();
+	Map<String, Integer> maxIndices = new HashMap<>();
 	private int frameLength = 10;
 	
 	Map<String, Double> similarities = new HashMap<>();
 	
+	public HashMap<String, HashMap<Integer, Double>> getFramewiseAudioValues() {
+		return framewiseAudioValues;
+	}
+
+	@SuppressWarnings("unlikely-arg-type")
 	public Map<String, Double> calculateStatsOfAllPairs(String queryPath) throws ClassNotFoundException, IOException {
 		Map<Integer, Double> distances = new HashMap<>();
 		String[] videoNames = {"flowers", "interview", "movie", "musicvideo", "sports", "starcraft", "traffic"};
@@ -54,9 +60,18 @@ public class AudioSemantics {
 			double simVal = (100 - ((distances.get(i) - minDist)/(maxDist - minDist)*100))*0.9;
 			similarities.put(videoNames[i], Constants.CONTRAST_PRIORITY * simVal);
 		}
+		Entry<String, Double> maxEntry = calculateMaxEntryString(similarities);
+		calculateHighestValues(framewiseAudioValues.get(maxEntry.getKey()), maxIndices.get(maxEntry.getKey()));
 		return similarities;
 	}
 	
+	private void calculateHighestValues(Map<Integer, Double> map, Integer index) {
+		for(int i = index; i < index + 150; i++) {
+			map.put(i, (map.get(i) / 15) * 22);
+		}
+		
+	}
+
 	public ArrayList<Double> audioAnalysis(String filePath) {
 		File file = new File(filePath);
 		try {
@@ -111,22 +126,59 @@ public class AudioSemantics {
         ArrayList<Double> databaseAudioFrames = (ArrayList<Double>) iis.readObject();
         iis.close();
 		
-        Entry<Integer, Double> minError = calculateMinError(queryFrames, databaseAudioFrames);
+        Map<Integer, Double> minErrorValues = calculateMinError(queryFrames, databaseAudioFrames);
+        Entry<Integer, Double> minError = calculateMinEntry(minErrorValues);
         
-        Entry<Integer, Double> maxError = calculateMaxEntry(errorValues);
-        for(Entry<Integer, Double> entry : errorValues.entrySet()) {
-        	framewiseAudioValues.put(entry.getKey(), (100 - (entry.getValue()/maxError.getValue())*100)*Constants.AUDIO_PRIORITY);
-        }
+        // calculating the graph values 
+        ArrayList<Double> graphValues = new ArrayList<>(databaseAudioFrames.subList(minError.getKey(), minError.getKey() + 150));
+        calculateGraphValues(graphValues, queryFrames, minError.getKey(), databaseAudioPath);
         
+        // calculating percentage
+//        Entry<Integer, Double> maxError = calculateMaxEntry(minErrorValues);
+//        Map<Integer, Double> framewisePercentage = new HashMap<>();
+//        for(Entry<Integer, Double> entry : minErrorValues.entrySet()) {
+//        	framewisePercentage.put(entry.getKey(), (100 - (entry.getValue()/maxError.getValue())*100)*Constants.AUDIO_PRIORITY);
+//        }
+//        framewiseAudioValues.put(databaseAudioPath, framewisePercentage);
         
-//        double matchPercentage = 100 - Math.round(minError.getValue()*10000.0)/100.0;
         
 		return minError.getValue();
-				
- 		
 	}
 	
-	public Entry<Integer, Double> calculateMinError(ArrayList<Double> queryFrames, ArrayList<Double> databaseAudioFrames) {
+	public void calculateGraphValues(ArrayList<Double> graphValues, ArrayList<Double> queryFrames, int index, String databaseAudioPath) {
+		ArrayList<Double> values = new ArrayList<>();
+		for(int i = 0; i < 150; i++) {
+			double value = Math.abs(graphValues.get(i) - queryFrames.get(i));
+			values.add(value);	
+		}
+		
+		Double maxVal = Collections.max(values);
+		int maxIndex = values.indexOf(Collections.max(values)) + index;
+		maxIndices.put(databaseAudioPath, maxIndex);
+		for(int i = 0; i < 150; i++) {
+			values.set(i, (values.get(i)/maxVal) * 15);
+		}
+		HashMap<Integer, Double> framewisePercentage = new HashMap<>();
+		initializeFinalMap(framewisePercentage);
+		
+		for(int i = 0; i < 150; i++) {
+			framewisePercentage.put(index + i, values.get(i));
+		}
+		
+		framewiseAudioValues.put(databaseAudioPath, framewisePercentage);
+		
+	}
+	
+	private void initializeFinalMap(Map<Integer, Double> frameWiseSimilarity) {
+		Random r = new Random();
+		for(int i = 0; i < 600; i++) {
+			double randomValue = 0 + (5 - 0) * r.nextDouble();
+			frameWiseSimilarity.put(i,  randomValue);
+		}
+	}
+
+	public Map<Integer, Double> calculateMinError(ArrayList<Double> queryFrames, ArrayList<Double> databaseAudioFrames) {
+		Map<Integer, Double> errorValues = new HashMap<>();
 		for(int i = 0; i < databaseAudioFrames.size() - queryFrames.size(); i++) {
 			double sum = 0.0;
 			for(int j = 0; j < queryFrames.size(); j++) {
@@ -164,7 +216,7 @@ public class AudioSemantics {
 //		}
 		
 		
-		return calculateMinEntry(errorValues);
+		return errorValues;
 	}
 
 	private Entry<Integer, Double> calculateMinEntry(Map<Integer, Double> values) {
@@ -175,6 +227,16 @@ public class AudioSemantics {
 		    }
 		}
 		return min;
+	}
+	
+	private Entry<String, Double> calculateMaxEntryString(Map<String, Double> values) {
+		Entry<String, Double> max = null;
+		for(Entry<String, Double> entry : values.entrySet()) {
+		    if (max == null || max.getValue() < entry.getValue()) {
+		        max = entry;
+		    }
+		}
+		return max;
 	}
 	
 	private Entry<Integer, Double> calculateMaxEntry(Map<Integer, Double> values) {
